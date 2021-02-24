b3920a6e082b8f2b56ed69eca996ac1bd8f1a245
Pranav Prakash Menon <ppmenon02@gmail.com>
2021-02-24 17:11:53 +0530

    revamp

---
+++ README.md
@@ -0,0 +1,9 @@
+# TODO App
+
+this app has been build with Vanilla JavaScript and CSS
+
+the todos are saved in localStorage
+
+# Running the App
+
+Just open the index.html file in your browser
--- app.js
+++ app.js
@@ -1,119 +1,106 @@
-// selectors
-const todoInput = document.querySelector("input");
-const todoButton = document.querySelector("button");
-const todoList = document.querySelector(".todoList");
-const form = document.querySelector("#input-form");
-
-// event listeners
-document.addEventListener("DOMContentLoaded", getTodos);
-form.addEventListener("submit", addtodo);
-todoList.addEventListener("click", deleteCheck);
-
-//disbled submit button
-todoInput.onkeyup = () => {
-  if (todoInput.value.length > 0) {
-    todoButton.disabled = false;
-  } else {
-    document.querySelector("button").disabled = true;
+(function () {
+  /**
+   *
+   * @param {HTMLElement} e
+   * @param {*} cls
+   * equivalent to e.classList.toggle(cls)
+   */
+  function debounce(func, wait) {
+    let timeout;
+    return function () {
+      const context = this;
+      const args = arguments;
+      const later = () => {
+        timeout = null;
+        func.apply(context, args);
+      };
+      const callNow = !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
   }
-};
-// functions
-function addtodo(event) {
-  event.preventDefault();
-  const val = todoInput.value;
-  if (!val.trim().length) return; 
-
-
+  function toggleClass(e, cls) {
+    if (e.classList.contains(cls)) {
+      return e.classList.remove(cls);
+    }
+    return e.classList.add(cls);
+  }
   
-
-  // create todo div
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todoDiv");
-
-  // create li
-  const todoLi = document.createElement("li");
-  todoLi.innerText = val;
-  todoLi.classList.add("todoItem");
-  todoDiv.appendChild(todoLi);
-
-  // add todo to localstorage
-
-  saveLocalTodos(val);
-
-  // check button
-  const checkButton = document.createElement("button");
-  checkButton.innerHTML = '<i class="fas fa-check"></i>';
-  checkButton.classList.add("checkButton");
-  todoDiv.appendChild(checkButton);
-
-  // delete button
-  const deleteButton = document.createElement("button");
-  deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
-  deleteButton.classList.add("deleteButton");
-  todoDiv.appendChild(deleteButton);
-
-  // append to list
-  todoList.appendChild(todoDiv);
-  // clear todo input value
-  todoInput.value = "";
-}
-
-//function2
-
-function deleteCheck(e) {
-  // for delete
-  const item = e.target;
-  if (item.classList[0] === "deleteButton") {
-    const todo = item.parentElement;
-    removeLocalTodos(todo);
-    todo.remove();
-  }
-
-  // for check
-  if (item.classList[0] === "checkButton") {
-    todo = item.parentElement;
-    todo.classList.add("completed");
-    // functions waits for transition to end -->
-    removeLocalTodos(todo);
-    todo.addEventListener("transitionend", function () {
-      todo.remove();
+  function setupInputElement(inp) {
+    const id = inp.dataset.id;
+    const getTodo = () => getTodoObj()[id];
+    inp.addEventListener(
+      "input",
+      debounce(() => {
+        const value = inp.value;
+        if (value && value.trim()) {
+          const t = getTodo();
+          t.text = value;
+          const todos = getTodoObj();
+          todos[id] = t;
+          saveTodoToLocalStorage(todos);
+        }
+      }, 300)
+    );
+    inp.addEventListener("blur", () => {
+      const val = inp.value;
+      console.log(val);
+      if (!val) {
+        const el = document.querySelector("[data-deletes='" + id + "'");
+        el.click();
+      }
     });
   }
-}
+  // selectors
+  const todoInput = document.querySelector("input");
+  const todoList = document.querySelector(".todoList");
+  const form = document.querySelector("#input-form");
 
-//later addition:
+  // event listeners
+  document.addEventListener("DOMContentLoaded", getTodos);
+  form.addEventListener("submit", addtodo);
 
-// adding localstorage feature
-
-function saveLocalTodos(todo) {
-  // check local storage
-
-  let todos;
-  if (localStorage.getItem("todos") === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
-  }
-  todos.push(todo);
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-function getTodos() {
-  let todos;
-  if (localStorage.getItem("todos") === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
+  // functions
+  function addtodo(event) {
+    event.preventDefault();
+    const val = todoInput.value;
+    if (!val.trim().length) return;
+    const obj = { text: val, completed: false, id: +new Date() };
+    saveLocalTodos(obj);
+    todoInput.value = "";
+    createNewTodoElement(obj);
   }
 
-  todos.forEach(function (todo) {
+  function checkTodo(e) {
+    const currentTarget = e.currentTarget;
+    const id = currentTarget.dataset.id;
+    toggleCompletedState(id);
+    toggleClass(
+      document.querySelector("input[data-id='" + id + "']"),
+      "completed"
+    );
+  }
+  function deleteTodo(e) {
+    const currentTarget = e.currentTarget;
+    const id = currentTarget.dataset.id;
+    removeLocalTodos(id);
+    const container = document.querySelector("[data-owner-of='" + id + "']");
+    container.remove();
+  }
+  function createNewTodoElement(todo) {
     // create todo div
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todoDiv");
 
     // create li
     const todoLi = document.createElement("li");
-    todoLi.innerText = todo;
+    const inp = document.createElement("input");
+    inp.className = "auto-updating-input";
+    inp.value = todo.text;
+    inp.setAttribute("data-id", todo.id);
+    setupInputElement(inp);
+    todoLi.appendChild(inp);
     todoLi.classList.add("todoItem");
     todoDiv.appendChild(todoLi);
 
@@ -121,28 +108,55 @@ function getTodos() {
     const checkButton = document.createElement("button");
     checkButton.innerHTML = '<i class="fas fa-check"></i>';
     checkButton.classList.add("checkButton");
+    checkButton.setAttribute("data-id", todo.id);
+    checkButton.addEventListener("click", checkTodo);
     todoDiv.appendChild(checkButton);
-
+    todoDiv.setAttribute("data-owner-of", todo.id);
     // delete button
     const deleteButton = document.createElement("button");
     deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
     deleteButton.classList.add("deleteButton");
+    deleteButton.setAttribute("data-id", todo.id);
+    deleteButton.setAttribute("data-deletes", todo.id);
+    deleteButton.addEventListener("click", deleteTodo);
     todoDiv.appendChild(deleteButton);
-
     // append to list
+    if (todo.completed) inp.classList.add("completed");
     todoList.appendChild(todoDiv);
-  });
-}
-
-function removeLocalTodos(todo) {
-  let todos;
-  if (localStorage.getItem("todos") === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
   }
 
-  const todoIndex = todo.children[0].innerText;
-  todos.splice(todos.indexOf(todoIndex), 1);
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
+  function saveLocalTodos(todo) {
+    const todos = getTodoObj();
+    todos[todo.id] = todo;
+    saveTodoToLocalStorage(todos);
+  }
+
+  function getTodos() {
+    const todos = getTodoObj();
+    Object.values(todos).forEach(createNewTodoElement);
+  }
+
+  function removeLocalTodos(id) {
+    const todos = getTodoObj();
+    // using undefined and then stringifying automatically removes it as json doesn't allow undef
+    todos[id] = undefined;
+    saveTodoToLocalStorage(todos);
+  }
+  function toggleCompletedState(id) {
+    const todos = getTodoObj();
+    todos[id].completed = !todos[id].completed;
+    saveTodoToLocalStorage(todos);
+  }
+  function getTodoObj() {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+      todos = {};
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    return todos;
+  }
+  function saveTodoToLocalStorage(todos) {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+})();
